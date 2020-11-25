ca234b38b0da7157c6c0dc5d7e4c53e128f3e847
Maggie Zhu <maggieqzhu@yahoo.com>
2020-11-25 16:46:25 -0500

    organizing

---
+++ other/parse_csv.py
@@ -0,0 +1,93 @@
+import csv
+from csv import reader, writer
+
+import requests
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from bs4 import BeautifulSoup
+import re
+import urllib.request
+from urllib.request import urlopen
+
+from time import sleep
+
+import nltk
+
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+driver = webdriver.Chrome('./chromedriver', options=option)
+
+
+def get_snapshot(url, count):
+
+    name = "app" + str(count) + ".png"
+    
+    driver.get(url)
+    sleep(1)
+    driver.get_screenshot_as_file('static/' + name)
+
+    return name
+
+
+with open('input.csv', 'r') as input, \
+    open('output.csv', 'w') as output:
+
+    fieldnames=['id','topic','name','site', 'screenshot', 'description', \
+        'publication', 'query', 'doi','title']
+
+    reader = csv.DictReader(input)
+    writer = csv.DictWriter(output, fieldnames=fieldnames)
+
+    writer.writeheader()
+
+    count = 1
+
+    apps = set()
+    
+    for line in reader: 
+
+        # GET DATASET NAME 
+        doi = line['doi']
+        doi = doi.split("g/")[1]
+        
+        url = "https://api.datacite.org/dois/" + doi
+
+        headers = {"Accept": "application/vnd.api+json"}
+
+        response = requests.request("GET", url, headers=headers)
+
+        title = response.json()['data']['attributes']['titles'][0]
+
+        print(title['title'])
+        
+        line['title'] = title['title']
+
+       
+        # GET SITE SCREENSHOT
+
+        # only get screenshot for app if it hasn't been done
+        if line['name'] not in apps:
+            line['screenshot'] = get_snapshot(line['site'], count)
+            line['id'] = str(count)
+            count += 1
+        
+
+        # GET APP DESCRIPTION
+        req = requests.get(line['site'])
+        soup = BeautifulSoup(req.text, 'html.parser')
+        topics = ['fire', 'flood', 'landslide']
+        for p in soup.find_all('p'):
+            for topic in topics:
+                if topic in p.get_text():
+                    sentence = p.get_text().split('.')
+                    
+        # line['description'] = sentence[0]
+
+        # add app to set
+        apps.add(line['name'])
+        
+        
+        writer.writerow(line)
+
---
+++ other/snapshot.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+driver = webdriver.Chrome('./chromedriver', options=option)
+
+driver.get('https://disc.gsfc.nasa.gov/information/data-in-action?title=Cyclone%20Harold%20Tracks%20Through%20Idyllic%20Islands%20of%20the%20South%20Pacific')
+sleep(1)
+
+count = 2
+name = "test.png"
+# name = "app" + str(count) + ".png"
+
+driver.get_screenshot_as_file('static/' + name)
+driver.close()
+driver.quit()
+
+
+
+# how to make the website not pop up? 
