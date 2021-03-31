981714cc31e4c67c32ea39b4208ec3029d52889f
Sophia Xia <sophia.xia67@myhunter.cuny.edu>
2021-03-31 15:51:30 -0400

    get screenshot for apps without it

--- util/snapshot.py
+++ util/snapshot.py
@@ -1,23 +1,13 @@
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from s3_functions import s3Functions
+from graph_db import GraphDB
 
+s3 = s3Functions()
+g = GraphDB()
 
-option = webdriver.ChromeOptions()
-option.add_argument('headless')
-driver = webdriver.Chrome('./chromedriver', options=option)
-
-driver.get('https://disc.gsfc.nasa.gov/information/data-in-action?title=Cyclone%20Harold%20Tracks%20Through%20Idyllic%20Islands%20of%20the%20South%20Pacific')
-sleep(1)
-
-count = 2
-name = "test.png"
-# name = "app" + str(count) + ".png"
-
-driver.get_screenshot_as_file('static/' + name)
-driver.close()
-driver.quit()
-
-
-
-# how to make the website not pop up? 
+NA = g.get_apps_without_screenshot()
+for app in NA:
+    name = app['name'][0]
+    site = app['site'][0]
+    print(name, site)
+    screenshot = s3.upload_image_from_url('test-bucket-parth', site)
+    g.update_app_property(name, 'screenshot', screenshot)
