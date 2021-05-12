95ebd887878d5a4297c4019616a8297444b51ebe
vinny <vinny.inverso@gmail.com>
2021-05-12 10:58:06 -0400

    Document what load_graph is doing

--- util/load_graph.py
+++ util/load_graph.py
@@ -3,6 +3,8 @@
 load_graph.py
 Load a set of application-dataset relationships in CSV form into a Neptune graph database
 """
+from dotenv import load_dotenv
+load_dotenv()
 import sys
 sys.path.append("../")
 import csv
@@ -26,33 +28,65 @@ def db_input_csv(input_file):
     # TAKE A SNAPSHOT OF YOUR NEPTUNE DB BEFORE RUNNING
     graph = GraphDB()
     graph.clear_database()
-    # load csv file
     with open(input_file, 'r') as file:
-        # initiate csv reader
         reader = csv.DictReader(file)
-        # loop through every line in csv file
         for line in reader:
+            # add a default classification
+            # 'unclassified',
+            # 'Software',
+            # 'Research'
             if not 'type' in line.keys():
                 line['type'] = 'unclassified'
+            
+            # parse topics (app can belong to many topics[])
             line['topic'] = re.sub("\]|\[|\'", '', line['topic'])
             line['topic'] = line['topic'].split(',')
+            
+            # parse types (app node can have multiple types[])
             line['type'] = re.sub("\]|\[|\'", '', line['type'])
             line['type'] = line['type'].split(',')
+            
+            # add each topic[], before adding the app
             for index, t in enumerate(line['topic']):
                 line['topic'][index] = t.strip()
                 print(graph.add_topic(line['topic'][index]))
+            
+            # add the application to the graph
+            # columns: name, site, screenshot, publication, description, 
+            # app_discoverer, app_verifier, app_verified, type[], topic[]
             if 'app_discoverer' in line.keys():
-                graph.add_app(line, discoverer=line['app_discoverer'], verified=('True'==line['app_verified']), verifier=line['app_verifier'])
+                graph.add_app(
+                    app=line, 
+                    discoverer=line['app_discoverer'], 
+                    verified=('True'==line['app_verified']), 
+                    verifier=line['app_verifier'])
             else: 
-                graph.add_app(line, discoverer='0000-0002-3708-5496', verified=True, verifier='0000-0002-3708-5496') # put your orcid Id in for discoverer and verifier
-                #graph.add_app(line)
-            graph.add_dataset(line)
+                # put your orcid Id in for discoverer and verifier
+                graph.add_app(
+                    app=line, 
+                    discoverer='0000-0002-3708-5496', 
+                    verified=True, 
+                    verifier='0000-0002-3708-5496') 
+            
+            # add the dataset to the graph
+            # columns: doi, title
+            graph.add_dataset(dataset=line)
+            
+            # connect the dataset to the app
+            # columns: annotation, discoverer, verifier, verified
             if 'discoverer' in line.keys():
                 print('new line:\n', line)
                 print('verifier:\n', line['verified'])
-                graph.add_relationship(line['site'], line['doi'], discoverer=line['discoverer'], verified='True'==line['verified'], verifier=line['verifier'], annotation=line['annotation'])
+                graph.add_relationship(
+                    site=line['site'], 
+                    doi=line['doi'], 
+                    discoverer=line['discoverer'], 
+                    verified='True'==line['verified'], 
+                    verifier=line['verifier'], 
+                    annotation=line['annotation'])
             else:
-                graph.add_relationship(line['site'], line['doi'])
+                graph.add_relationship(site=line['site'], doi=line['doi'])
+    
     # counts vertices, used for troubleshooting purposes
     print(graph.get_vertex_count())
     print(graph.get_edge_count())
