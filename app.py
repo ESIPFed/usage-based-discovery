484aaf128478e4a603ac445825a2d351aaafe761
vinny <vinny.inverso@gmail.com>
2022-05-04 15:05:28 -0400

    Simplify app run code and tweak for running in Docker

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+import os
 import app_factory
+from util import env_helper
 
-app = app_factory.create_flask_app()
+app = None
+is_docker = os.environ.get('IS_DOCKER') == 'true'
+is_dev = os.environ.get('FLASK_ENV') == 'development'
+if not (is_docker or is_dev):
+    app = app_factory.create_flask_app()
+
+if __name__ == '__main__':
+    # this should only be invoked in non-production modes (FLASK_ENV != production)
+    neptune_endpoint = 'ws://localhost:8183/gremlin'
+    host = None
+    if is_docker:
+        neptune_endpoint = 'ws://host.docker.internal:8183/gremlin'
+        host='0.0.0.0'
+    env_helper.load_vars(flask_env='development', neptune_endpoint=neptune_endpoint)
+    app = app_factory.create_flask_app()
+    
+    app.run(debug=True, host=host)
